f964405aad6fe908a6ae4438e6324f0fca101a73
allecosta <alle_costa10@outlook.com>
2022-09-27 18:08:33 -0300

    Adding database connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,11 +1,12 @@
 <?php
 
-$DATABASE_HOST = "localhost";
-$DATABASE_USER = "root";
-$DATABASE_PASS = "matrix0101";
-$DATABASE_NAME = "my_hero_academia";
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_USER', '###');
+define('DATABASE_PAS', '###');
+define('DATABASE_NAME', 'my_hero_academia');
 
-$conn = mysqli_connect("$DATABASE_HOST", "$DATABASE_USER", "$DATABASE_PASS", "$DATABASE_NAME");
+
+$conn = mysqli_connect('DATABASE_HOST', 'DATABASE_USER', 'DATABASE_PASS', 'DATABASE_NAME');
 
 //mysqli_set_charset($conn, "utf-8");
 
